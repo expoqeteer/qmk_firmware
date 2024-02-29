dd141709ccc96dad2a8adab1c14fbb800d7ca3e6
Bryan Mason <bmason@bmason.com>
2024-02-29 15:54:54 -0800

    Drop SHIFTv2 customizations:
    - Add PgUp/PgDown/Home/End to Fn+Up/Down/Left/Right
    - Enable dynamic tapping term
    - Set default to 200
    - Use keypad "+", "-", and "ENTER" to increase/decrease/show current tapping term

--- keyboards/drop/shift/keymaps/bmason/rules.mk
+++ keyboards/drop/shift/keymaps/bmason/rules.mk
@@ -1,1 +1,5 @@
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_led_matrix.md#custom-led-matrix-effects-idcustom-led-matrix-effects
 RGB_MATRIX_CUSTOM_USER = yes
+
+# https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md
+DYNAMIC_TAPPING_TERM_ENABLE = yes
